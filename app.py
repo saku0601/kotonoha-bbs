980e47c510748d72221a03dc96971908f11cab5f
JEEPNARITA <JEEPNARITA@example.com>
2025-07-06 21:06:44 +0900

    DB初期化用にdb.create_all()を追加

--- app.py
+++ app.py
@@ -206,4 +206,5 @@ if __name__ == '__main__':
             db.session.add(admin)
             db.session.commit()
             print('管理者ユーザーadminを作成しました')
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
